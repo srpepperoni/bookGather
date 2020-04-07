3cb7cac8062f4ff1eeb905de5e55caa7cd137c55
srpepperoni <jaimeyera@gmail.com>
2020-04-08 00:58:20 +0200

    Added new endpoint to create pdf from image files

--- src/main/java/com/project/books/bookGather/controller/BookController.java
+++ src/main/java/com/project/books/bookGather/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.project.books.bookGather.controller;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.project.books.bookGather.service.BookService;
 
 @RestController
@@ -24,19 +29,41 @@ public class BookController {
 	}
 
 	@PostMapping(path = { "/generateFile" })
-	public void generateFileFromHAR(@RequestParam String origin, @RequestParam String destiny, @RequestParam String identification) {
+	public void generateFileFromHAR(@RequestParam String origin, @RequestParam String destiny,
+			@RequestParam String identification) {
 		List<String> urls = new ArrayList<String>();
 		System.out.println("------------------------");
 		System.out.println("origin " + origin);
 		System.out.println("destiny " + destiny);
-		
+
 		// Saco todas las entries del fichero
 		JSONArray entries = bookService.createEntries(origin);
-		
+
 		// Guardo la info de las entries con Request tipo GET en urls
 		bookService.getRequestInfo(identification, entries, urls);
-		
+
 		bookService.saveImages(urls, destiny);
 	}
-	
+
+	@PostMapping(path = { "/createPDF" })
+	public void createPDF(@RequestParam String imagesPath, @RequestParam int elements, @RequestParam String bookName) {
+		try {
+			Document document = new Document(PageSize.A4);
+			String output = imagesPath + bookName + ".pdf";
+			String input = "";
+			FileOutputStream fos = new FileOutputStream(output);
+			PdfWriter writer = PdfWriter.getInstance(document, fos);
+			writer.open();
+			document.open();
+			for (int i = 1; i < elements + 1; i++) {
+				input = imagesPath + "PA" + i + ".jpg";
+				document.add(Image.getInstance(input));
+			}
+			document.close();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
